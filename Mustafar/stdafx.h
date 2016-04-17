9c847c816c0815e1e8b61c2a2245d1cf3fffc236
FabrizioCozza <fabrizio.cozza@gmail.com>
2016-04-17 19:25:41 -0300

    Add algoritmo 2 + modificaciones pequeñas
    Agregado algoritmo 2 (en cpp aparte por si acaso), movidos typedefs a
    stdafx.h, agregadas librerias string y vector (error en MustafarGraph)

--- Mustafar/stdafx.h
+++ Mustafar/stdafx.h
@@ -10,7 +10,19 @@
 #include <stdio.h>
 #include <tchar.h>
 
+#define DOUBLE_PRECISION 0
+#if DOUBLE_PRECISION
+typedef double REAL;
+#else
+typedef float REAL;
+#endif
+
+#define STEP_COUNT 10e+1
+#define LAMBDA_DEN 9e+4 // 9e+5 gives epsilon out of range 
+
 #define _USE_MATH_DEFINES
 #include <math.h>
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
