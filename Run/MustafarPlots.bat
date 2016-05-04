b500dd35bfcc00992c1bca42c22d3686704d64b7
nmerloleiva <nmerloleiva@gmail.com>
2016-05-04 00:52:20 -0300

    Run Dir

---
+++ Run/MustafarPlots.bat
@@ -0,0 +1,4 @@
+Mustafar.exe -alg 1 -steps 100 -dp 0 -print 1 -scale 50000000 -width 3508 -height 2480
+Mustafar.exe -alg 2 -steps 100 -dp 0 -print 1 -scale 50000000 -width 3508 -height 2480
+Mustafar.exe -alg 1 -steps 100000000 -dp 0 -print 1 -scale 50000000 -width 3508 -height 2480
+Mustafar.exe -alg 2 -steps 10000000 -dp 0 -print 1 -scale 50000000 -width 3508 -height 2480
---
+++ Run/MustafarRuns.bat
@@ -0,0 +1,21 @@
+Mustafar.exe -alg 1 -steps 100
+Mustafar.exe -alg 1 -steps 1000
+Mustafar.exe -alg 1 -steps 10000
+Mustafar.exe -alg 1 -steps 100000
+Mustafar.exe -alg 1 -steps 1000000
+Mustafar.exe -alg 1 -steps 10000000
+Mustafar.exe -alg 1 -steps 100000000
+Mustafar.exe -alg 1 -steps 1000000000
+Mustafar.exe -alg 1 -steps 10000000000
+Mustafar.exe -alg 1 -steps 100000000000
+
+Mustafar.exe -alg 2 -steps 100
+Mustafar.exe -alg 2 -steps 1000
+Mustafar.exe -alg 2 -steps 10000
+Mustafar.exe -alg 2 -steps 100000
+Mustafar.exe -alg 2 -steps 1000000
+Mustafar.exe -alg 2 -steps 10000000
+Mustafar.exe -alg 2 -steps 100000000
+Mustafar.exe -alg 2 -steps 1000000000
+Mustafar.exe -alg 2 -steps 10000000000
+Mustafar.exe -alg 2 -steps 100000000000
