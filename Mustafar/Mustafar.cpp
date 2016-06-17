8bab1039c40c4b5d99be40f745b5e674f52dd105
FabrizioCozza <fabrizio.cozza@gmail.com>
2016-06-17 19:06:57 -0300

    Añadida integral trapecio

--- Mustafar/Mustafar.cpp
+++ Mustafar/Mustafar.cpp
@@ -312,8 +312,8 @@ void pointsInterpolator() //seria para ejecutar el main este proceso
 	_getch();
 }
 
-double rectangularIntegral(double(*f)(double x), double a, double b, int steps) {
-	double step = (b - a) / n;  // width of each small rectangle
+double rectangularIntegral(double(*f)(double x), double a, double b, int steps) { //function, I[a,b]
+	double step = (b - a) / steps;  // width of each small rectangle
 	double area = 0.0;  // signed area
 	for (int i = 0; i < steps; i++) {
 		area += f(a + (i + 0.5) * step) * step; // sum up each small rectangle
@@ -321,9 +321,47 @@ double rectangularIntegral(double(*f)(double x), double a, double b, int steps) 
 	return area;
 }
 
-double calculateIntegral()
+float f(float x) //function example
 {
-	return rectangularIntegral(cos, 0, M_PI / 2, 10); //function, I[a.b], steps
+	return(1 / (1 + pow(x, 2)));
+}
+
+void trapezoidalIntegral()
+{
+	int i, n;
+	float x0, xn, h, y[20], so, se, ans, x[20];
+	printf("\n Enter values of x0,xn,h:\n");
+	scanf_s("%f%f%f", &x0, &xn, &h);
+	n = (xn - x0) / h;
+	if (n % 2 == 1)
+	{
+		n = n + 1;
+	}
+	h = (xn - x0) / n;
+	printf("\nrefined value of n and h are:%d  %f\n", n, h);
+	printf("\n Y values \n");
+	for (i = 0; i <= n; i++)
+	{
+		x[i] = x0 + i*h;
+		y[i] = f(x[i]);
+		printf("\n%f\n", y[i]);
+	}
+	so = 0;
+	se = 0;
+	for (i = 1; i<n; i++)
+	{
+		if (i % 2 == 1)
+		{
+			so = so + y[i];
+		}
+		else
+		{
+			se = se + y[i];
+		}
+	}
+	ans = h / 3 * (y[0] + y[n] + 4 * so + 2 * se);
+	printf("\nfinal integration is %f", ans);
+	_getch();
 }
 
 int main(int argc, char* argv[])
